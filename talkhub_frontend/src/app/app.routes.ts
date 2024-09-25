4d44c1ac9d77c5d1b21e1bb43c480e4fbcb7ee53
jdrms <jesurodriguez@utb.edu.co>
2024-09-24 19:14:34 -0500

    about and start components

--- talkhub_frontend/src/app/app.routes.ts
+++ talkhub_frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { StartComponent } from './pages/start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '',
+    component: StartComponent,
+  }
+];
