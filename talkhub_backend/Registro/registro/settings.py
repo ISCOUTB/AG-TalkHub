ce7736e0815e2d07177b004af96ef1851fa99f25
EmmanuelS23 <159738675+EmmanuelS23@users.noreply.github.com>
2024-09-29 23:18:55 -0500

    Bd changes2

--- talkhub_backend/Registro/registro/settings.py
+++ talkhub_backend/Registro/registro/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'registro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'registroUsuarios',
-        'USER': 'superusuario',
-        'PASSWORD': 'superusuario1',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- talkhub_backend/Registro/users/views.py
+++ talkhub_backend/Registro/users/views.py
@@ -9,7 +9,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Tu cuenta ha sido creada, {username}!')
-            return redirect('login')  
+            return redirect('login')  # Asegúrate de tener configurada la vista de login
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
