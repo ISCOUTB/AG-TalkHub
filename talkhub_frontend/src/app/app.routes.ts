4f3ad6f3b7976b05d17e729a715de56d91887886
Juan Caro <jcarosimanca@gmail.com>
2024-09-29 19:49:06 -0500

    Implementation of register and login view

--- talkhub_frontend/src/app/app.routes.ts
+++ talkhub_frontend/src/app/app.routes.ts
@@ -1,12 +1,23 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { StartComponent } from './pages/start/start.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
   {
     path: 'about',
     component: AboutComponent,
   },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: '',
     component: StartComponent,
