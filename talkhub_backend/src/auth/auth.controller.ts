a0e2b1f022088a6a2530c238371781412fc269f7
jdrms <jesurodriguez@utb.edu.co>
2024-10-02 21:23:44 -0500

    Login implemented

--- talkhub_backend/src/auth/auth.controller.ts
+++ talkhub_backend/src/auth/auth.controller.ts
@@ -13,7 +13,12 @@ import { AuthGuard } from './auth.guard';
 import { Public } from './public-route.metadata';
 import { UsersService } from 'src/users/users.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { TokenDto } from './dto/token.dto';
 import { ProfileDto } from './dto/profile.dto';
@@ -29,6 +34,10 @@ export class AuthController {
 
   @Public()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    description: 'Login to the application',
+    operationId: 'login',
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 200, description: 'Login successful', type: TokenDto })
   @Post('login')
@@ -37,6 +46,10 @@ export class AuthController {
   }
 
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    description: 'Get user profile data',
+    operationId: 'profile',
+  })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
   @ApiResponse({ status: 200, description: 'Profile data', type: ProfileDto })
   @Get('profile')
@@ -45,6 +58,10 @@ export class AuthController {
   }
 
   @Public()
+  @ApiOperation({
+    description: 'Register a new user',
+    operationId: 'register',
+  })
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
   async register(@Body() registerDto: CreateUserDto) {
--- talkhub_frontend/src/app/pages/login/login.component.ts
+++ talkhub_frontend/src/app/pages/login/login.component.ts
@@ -1,13 +1,73 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
+import { AuthService, Configuration } from '../../api'; // Import Configuration
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
+/**
+ * Login component
+ */
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule], // Add CommonModule here
+  imports: [CommonModule, FormsModule, ReactiveFormsModule], // Add CommonModule here
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'], // Corrected to styleUrls
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  /**
+   * Create a new instance of the LoginComponent
+   * @param authService Auth service
+   * @param formBuilder Angular form builder
+   * @param config API Configuration
+   * @param router Router
+   */
+  constructor(
+    private readonly authService: AuthService,
+    private readonly formBuilder: FormBuilder,
+    private readonly config: Configuration,
+    private readonly router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  /**
+   * Login the user
+   */
+  login() {
+    // Call the login API
+    this.authService
+      .login({
+        email: this.loginForm.value.email || '',
+        password: this.loginForm.value.password || '',
+      })
+      .subscribe({
+        next: (response) => {
+          // Assuming the response contains a token (e.g., response.token)
+          const token = response.access_token;
+
+          // Store the token in localStorage
+          localStorage.setItem('access_token', token);
+
+          // Update the ApiModule's Configuration with the new token
+          this.config.credentials['bearer'] = () => token;
+
+          // Redirect to the home page
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          window.alert('Login failed');
+        },
+      });
+  }
 }
