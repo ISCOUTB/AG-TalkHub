bcd8cc01d13af72f35751aa0a1b0fa58eb3a4846
jdrms <jesurodriguez@utb.edu.co>
2024-10-02 19:13:43 -0500

    Refactor package dependencies and add Swagger documentation

--- talkhub_backend/src/main.ts
+++ talkhub_backend/src/main.ts
@@ -2,10 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Talkhub API')
+    .setDescription('This is a forum API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
