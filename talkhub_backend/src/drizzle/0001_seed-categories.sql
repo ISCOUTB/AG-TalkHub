bee6c082286a88864c645c99948cde78d8a71346
jdrms <jesurodriguez@utb.edu.co>
2024-10-22 20:30:39 -0500

    Update docker-compose.yml and Dockerfile to properly install dependencies and added default categories

---
+++ talkhub_backend/src/drizzle/0001_seed-categories.sql
@@ -0,0 +1,7 @@
+INSERT INTO `categories` (`name`, `description`) 
+VALUES 
+('General', 'General discussion'),
+('Videogames', 'Discussion of anything related to videogames! clips, guides, news and more!'),
+('Superheroes', 'All about superheroes, comics, movies, series and more!'),
+('Social Media', 'Discussion of social media platforms, news and trends'),
+('Tech', 'Discussion of innovations and changes to technology');
